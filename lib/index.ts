97155228c600682755fa2887476def13c98a65c0
mgechev <mgechev@gmail.com>
2016-03-18 23:18:10 +0200

    refactor: update implementation

--- lib/index.ts
+++ lib/index.ts
@@ -8,14 +8,15 @@ export interface ValueCallback {
 
 export interface AdaptConfig {
   name?: string | NameCallback;
-  value?: any | ValueCallback
+  denormalize?: any | ValueCallback;
+  normalize?: any | ValueCallback;
   type?: Function;
   hide?: boolean;
 }
 
 export const Adapt = (config: AdaptConfig) => {
   return (objType, prop) => {
-    const currentMetadata = metadata.get(objType.constructor) || new TypeAdapter();
+    const currentMetadata = metadata.get(objType.constructor) || new TypeAdapter(config);
     const fieldAdapter = new SerializableField(config, objType, prop);
     currentMetadata.fields.set(prop, fieldAdapter);
     metadata.set(objType.constructor, currentMetadata);
@@ -24,28 +25,39 @@ export const Adapt = (config: AdaptConfig) => {
 
 export const denormalize = (obj: Object, objType?: Function) => {
   objType = objType || obj.constructor;
-  const adapter = metadata.get(objType);
-  if (!adapter) {
-    return JSON.parse(JSON.stringify(obj));
-  }
-  return adapter.denormalize(obj);
+  return transformHelper(objType).denormalize(obj);
 };
 
 export const normalize = (obj: Object, objType: Function) => {
+  return transformHelper(objType).normalize(obj);
 };
 
 const metadata = new Map<Function, TypeAdapter>();
+
+const basicTransformer = obj => JSON.parse(JSON.stringify(obj));
+const transformHelper = (objType): ITypeAdapter => {
+  const adapter = metadata.get(objType);
+  if (adapter) {
+    return adapter;
+  } else {
+    return {
+      normalize: basicTransformer,
+      denormalize: basicTransformer
+    };
+  }
+};
+
 class SerializableField {
   constructor(private config: AdaptConfig, private objType: Function, private field: string) {}
   denormalize(obj: Object, target: Object): void {
     if (this.config.hide) return;
     const targetField = this.getTargetFieldName(obj, target);
-    const propValueCtr = obj[this.field].constructor;
+    const propValueCtr = this.config.type || obj[this.field].constructor;
     let fieldValue;
     if (!this.config.type && !metadata.get(propValueCtr)) {
-      fieldValue = this.processPrimitiveField(obj, target);
+      fieldValue = this.processPrimitiveField(obj, target, this.config.denormalize);
     } else {
-      fieldValue = this.processComplexField(propValueCtr, obj[this.field]);
+      fieldValue = transformHelper(propValueCtr).denormalize(obj[this.field]);
     }
     target[targetField] = fieldValue;
   }
@@ -60,30 +72,41 @@ class SerializableField {
     }
     return targetField;
   }
-  private processPrimitiveField(obj: Object, target: Object): Object {
+  private processPrimitiveField(obj: Object, target: Object, transform: ValueCallback | any): Object {
     let objField = this.field;
     const config = this.config;
-    if (typeof config.value === 'function') {
-      return (<ValueCallback>config.value)(obj, objField);
-    } else if (config.value) {
-      return config.value;
+    if (typeof transform === 'function') {
+      return (<ValueCallback>transform)(obj, objField);
+    } else if (transform) {
+      return transform;
     } else {
       return obj[objField];
     }
   }
-  private processComplexField(ctr: Function, value: Object): Object {
-    return metadata.get(ctr).denormalize(value);
-  }
 }
 
-class TypeAdapter {
+interface ITypeAdapter {
+  denormalize(obj): Object;
+  normalize(obj): Object;
+}
+
+class TypeAdapter implements ITypeAdapter {
   fields: Map<string, SerializableField> = new Map<string, SerializableField>();
+  constructor(private config: AdaptConfig) {}
   denormalize(obj) {
     let result = {};
+    return this.translate(obj, result, (obj, result, fieldAdapter) => {
+      fieldAdapter.denormalize(obj, result);
+    });
+  }
+  normalize(obj) {
+    return obj;
+  }
+  private translate(obj, result, cb) {
     for (let name in obj) {
       const fieldAdapter = this.fields.get(name);
       if (fieldAdapter) {
-        fieldAdapter.denormalize(obj, result);
+        cb(obj, result, fieldAdapter);
       } else {
         result[name] = obj[name];
       }
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,9 +1,9 @@
-import {Adapt, denormalize} from '../lib/index';
+import {Adapt, denormalize, normalize} from '../lib/index';
 import * as chai from 'chai';
 
 class Foo {
   @Adapt({ name: 'baz' }) bar = 42;
-  @Adapt({ value: 1.618 }) foobar = 42;
+  @Adapt({ denormalize: 1.618 }) foobar = 42;
 }
 
 class Bar {
@@ -17,53 +17,67 @@ class Baz {
 
 class Foobar {
   @Adapt({ type: Foo }) foo = new Foo();
-  @Adapt({ name: 'baz' }) bar = new Bar();
+  @Adapt({ name: 'baz', type: Bar }) bar = new Bar();
 }
 
 const name = (obj: Object, name: string): string => {
   return 'baz';
 };
 
-const value = (obj: Object, field: string): any => {
+const denormalizeCb = (obj: Object, field: string): any => {
   return obj[field] + 1;
 };
 
 class Bazfoo {
-  @Adapt({ name, value }) foo = 42;
+  @Adapt({ name, denormalize: denormalizeCb }) foo = 42;
 }
 
 describe('Data adapter', () => {
-  it('should work without decorators set', () => {
-    const instance = { baz: 12 };
-    chai.expect(denormalize(instance, Bar)).deep.equal({ baz: 12 });
-  });
-  it('should work without type set', () => {
-    const instance = new Foo();
-    chai.expect(denormalize(instance, Foo)).deep.equal({ baz: 42, foobar: 1.618 });
-  });
-  it('should work with "name" and "value"', () => {
-    const instance = { bar: 12, foobar: 45 };
-    chai.expect(denormalize(instance, Foo)).deep.equal({ baz: 12, foobar: 1.618 });
-  });
-  it('should work with "hide"', () => {
-    const instance = new Baz();
-    chai.expect(denormalize(instance)).deep.equal({ baz: 42 });
-  });
-  it('should work with "complex" fields', () => {
-    const instance = new Foobar();
-    chai.expect(denormalize(instance)).deep.equal({
-      foo: {
-        baz: 42,
-        foobar: 1.618
-      },
-      baz: {
-        baz: 12
-      }
+  describe('denormalize', () => {
+    it('should work without decorators set', () => {
+      const instance = { baz: 12 };
+      chai.expect(denormalize(instance, Bar)).deep.equal({ baz: 12 });
+    });
+    it('should work without type set', () => {
+      const instance = new Foo();
+      chai.expect(denormalize(instance, Foo)).deep.equal({ baz: 42, foobar: 1.618 });
+    });
+    it('should work with "name" and "denormalize"', () => {
+      const instance = { bar: 12, foobar: 45 };
+      chai.expect(denormalize(instance, Foo)).deep.equal({ baz: 12, foobar: 1.618 });
+    });
+    it('should work with "hide"', () => {
+      const instance = new Baz();
+      chai.expect(denormalize(instance)).deep.equal({ baz: 42 });
+    });
+    it('should work with "complex" fields', () => {
+      const instance = new Foobar();
+      chai.expect(denormalize(instance)).deep.equal({
+        foo: {
+          baz: 42,
+          foobar: 1.618
+        },
+        baz: {
+          baz: 12
+        }
+      });
+    });
+    it('should work with callbacks', () => {
+      const instance = new Bazfoo();
+      chai.expect(denormalize(instance)).deep.equal({ baz: 43  });
     });
   });
-  it('should work with callbacks', () => {
-    const instance = new Bazfoo();
-    chai.expect(denormalize(instance)).deep.equal({ baz: 43  });
+  xdescribe('normalize', () => {
+    xit('should normalize simple objects', () => {
+      const instance = { baz: 12 };
+      const denormalized = denormalize(instance, Bar);
+      chai.expect(instance).deep.equal(normalize(denormalized, Bar));
+    });
+    xit('should work with complex objects', () => {
+      const instance = new Foobar();
+      const denormalized = denormalize(instance, Foobar);
+      chai.expect(instance).deep.equal(normalize(denormalized, Foobar));
+    });
   });
 });
 
